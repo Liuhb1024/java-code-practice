ee7e9c38cb3595f478c9149d95624d4632e0586b
liuhb-clanguage <2353836117@qq.com>
2023-10-14 21:53:56 +0800

    二叉树

--- java_code/DataStructure/binaryTree/BinaryTree.java
+++ java_code/DataStructure/binaryTree/BinaryTree.java
@@ -1,5 +1,8 @@
 package DataStructure.binaryTree;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author 刘浩彬
  * @date 2023/10/13
@@ -37,4 +40,200 @@ public class BinaryTree {
 
         return A;
     }
+
+    //前序遍历
+    public void preOrder(TreeNode root){
+        if (root == null) return;
+        System.out.print(root.val+" ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+    //中序遍历
+    public void inOrder(TreeNode root){
+        if (root == null) return;
+        inOrder(root.left);
+        System.out.print(root.val+" ");
+        inOrder(root.right);
+    }
+
+    //后序遍历
+    public void postOrder(TreeNode root){
+        if (root == null) return;
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.val+" ");
+
+    }
+
+
+    //将前序遍历的结果，存储到 List 中
+    /*List<TreeNode> ret = new ArrayList<>();
+
+    public List<TreeNode> preOrder2(TreeNode root){
+        if (root == null) return ret;
+        ret.add(root);
+        postOrder(root.left);
+        postOrder(root.right);
+        return ret;
+    }*/
+
+    //以上的递归并没有很好的利用好返回值
+
+    public List<TreeNode> preOrder2(TreeNode root){
+        List<TreeNode> ret = new ArrayList<>();
+        if (root == null) return ret;
+
+        ret.add(root);
+
+        List<TreeNode> leftTree = preOrder2(root.left);
+        ret.addAll(leftTree);
+        List<TreeNode> rightTree = preOrder2(root.right);
+        ret.addAll(rightTree);
+
+        return ret;
+    }
+
+    //中序遍历
+    public List<TreeNode> inOrder2(TreeNode root){
+        List<TreeNode> ret = new ArrayList<>();
+        if (root == null) return ret;
+
+
+        List<TreeNode> leftTree = inOrder2(root.left);
+        ret.addAll(leftTree);
+        ret.add(root);
+
+        List<TreeNode> rightTree = inOrder2(root.right);
+        ret.addAll(rightTree);
+
+        return ret;
+    }
+
+    //后序遍历
+    public List<TreeNode> postOrder2(TreeNode root){
+        List<TreeNode> ret = new ArrayList<>();
+        if (root == null) return ret;
+
+
+        List<TreeNode> leftTree = postOrder2(root.left);
+        ret.addAll(leftTree);
+
+        List<TreeNode> rightTree = postOrder2(root.right);
+        ret.addAll(rightTree);
+        ret.add(root);
+
+        return ret;
+    }
+
+    /**
+     * 二叉树中的基本操作
+     */
+
+    //获取树中节点的个数
+    public int size = 0;
+    public int nodeSize(TreeNode root){
+        if (root == null) return 0;
+        size++;
+
+        nodeSize(root.left);
+        nodeSize(root.right);
+        return size;
+    }
+
+    public int nodeSize2(TreeNode root){
+         if (root == null) return 0;
+
+         return nodeSize2(root.left)+nodeSize2(root.right)+1;
+    }
+
+
+    //获取叶子节点的个数  也就是度为 0 的节点
+    public int leafSize = 0;
+
+    public void getLeafSize(TreeNode root){
+        if (root == null) return;
+
+        if (root.left == null && root.right == null){
+            leafSize++;
+        }
+
+        getLeafSize(root.left);
+        getLeafSize(root.right);
+    }
+
+    public int getLeafSize2(TreeNode root){
+        if (root == null) return 0;
+
+        if (root.left == null && root.right == null){
+            return 1;
+        }
+
+        return getLeafSize2(root.left) + getLeafSize2(root.right);
+    }
+
+
+    //获取第 k 层节点的个数
+    public int getKlevelNodeCount(TreeNode root, int k){
+        if (root == null) return 0;
+        if (k == 1) return 1;
+
+        return getKlevelNodeCount(root.left,k-1)
+                + getKlevelNodeCount(root.right,k-1);
+    }
+
+    //获取二叉树的高度
+    //这种写法时间复杂度太大
+    public int getHeight(TreeNode root){
+        if (root == null) return 0;
+        return getHeight(root.left) > getHeight(root.right)
+                ? getHeight(root.left)+1
+                : getHeight(root.right)+1;
+    }
+
+    public int getHeight2(TreeNode root){
+        if (root == null) return 0;
+        int leftLeaf = getHeight2(root.left);
+        int rightLeaf = getHeight2(root.right);
+        return leftLeaf > rightLeaf
+                ? leftLeaf+1
+                : rightLeaf+1;
+    }
+
+    //检测值为 value 的元素是否存在
+    public boolean find(TreeNode root, char key){
+        if (root == null) return false;
+        if (root.val == key) return true;
+
+        boolean leftVal = find(root.left,key);
+        if (leftVal == true) return true;
+
+        boolean rightVal = find(root.right,key);
+        if (rightVal == true) return true;
+
+        return false;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- java_code/DataStructure/binaryTree/Test.java
+++ java_code/DataStructure/binaryTree/Test.java
@@ -1,5 +1,7 @@
 package DataStructure.binaryTree;
 
+import java.util.List;
+
 /**
  * @author 刘浩彬
  * @date 2023/10/13
@@ -7,7 +9,39 @@ package DataStructure.binaryTree;
 public class Test {
     public static void main(String[] args) {
         BinaryTree binaryTree = new BinaryTree();
-        BinaryTree.TreeNode ret = binaryTree.createTree();
+        BinaryTree.TreeNode root = binaryTree.createTree();
         System.out.println("===");
+
+        binaryTree.preOrder(root);
+        System.out.println();
+
+        binaryTree.inOrder(root);
+        System.out.println();
+
+        binaryTree.postOrder(root);
+        System.out.println();
+
+        System.out.println("=====================================");
+        List<BinaryTree.TreeNode> ret =  binaryTree.preOrder2(root);
+        for (BinaryTree.TreeNode x : ret){
+            System.out.print(x.val+" ");
+        }
+        System.out.println("=============");
+        binaryTree.nodeSize(root);
+        System.out.println("节点的个数："+binaryTree.size);
+        System.out.println("============================");
+        System.out.println("节点的个数："+binaryTree.nodeSize2(root));
+        System.out.println("=============================");
+        binaryTree.getLeafSize(root);
+        System.out.println("叶子节点的个数："+binaryTree.leafSize);
+        System.out.println("===============================");
+
+        //这里写 blog 的时候 debug 一下
+        boolean flg = binaryTree.find(root,'E');
+        if (flg == true){
+            System.out.println("找到这个节点");
+        }else{
+            System.out.println("找不到！");
+        }
     }
 }
